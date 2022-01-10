db667c305fa44cfacb093cace96925dd7e8b9310
Mihir Kanojia <kanojiamihir@gmail.com>
2022-01-10 23:56:01 +0530

    View notes activity created.

--- app/src/main/java/com/example/noteshub/ui/curd/NotesDetailsActivity.java
+++ app/src/main/java/com/example/noteshub/ui/curd/NotesDetailsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -13,6 +14,7 @@ import com.example.noteshub.databinding.ActivityCreateEditNotesBinding;
 public class NotesDetailsActivity extends AppCompatActivity {
 
     private ActivityCreateEditNotesBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,13 @@ public class NotesDetailsActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_notes_details);
         setContentView(binding.getRoot());
 
+        initComponents();
+
+    }
+
+    private void initComponents() {
+
+        binding.ibBackBtn.setOnClickListener(view -> onBackPressed());
 
 
 
